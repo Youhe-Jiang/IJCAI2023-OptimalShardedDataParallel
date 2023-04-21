81727506517b9c189597e8e7cd76a7b402683735
Youhe-Jiang <85312798+Youhe-Jiang@users.noreply.github.com>
2023-04-21 16:31:10 +0800

    Delete gpt directory

--- gpt/scripts/script_gpt2_osdp.sh
+++
@@ -1,9 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=8 --master_port 9999 train_gpt2_osdp.py \
---train_batch_size 1 \
---vocab_size 30522 \
---hidden_size 1536 \
---num_hidden_layer 48 \
---num_attention_heads 48 \
---fsdp_type OSDP \
---device_memory_limit 8192 \
---profile 1 \
